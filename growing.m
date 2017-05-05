57d73f08ae5413cd18175a30f747416b056bb5eb
Dulara De Zoysa <Dulara De Zoysa>
2017-05-05 17:09:05 +0530

    latest update

--- growing.m
+++ growing.m
@@ -1,30 +1,84 @@
-function [theta1,theta2,xo,yo] = growing(x,y,gradient)
+function [theta1,x_current,y_current] = growing(x,y,theta,level)
 
 length = 0;
-i=0;
 
+
+x_current = [];
+y_current = [];
+x_previous = [];
+y_previous = [];
+length_current = [];
+theta1 = [];
+
+for i = 1 : size(theta,2)
+    alpha = theta(i) - pi/(4*(2^level));
+    beta =  theta(i) + pi/(4*(2^level));
+    theta1 = horzcat(theta1, [alpha beta]);
+end
+
+for itr = 1:size(x,2)
+    x_current = horzcat(x_current,[x(itr) x(itr)]);
+    y_current = horzcat(y_current,[y(itr) y(itr)]);
+end
+
+i=0;
 while length<10
-    if(gradient==pi/2)
-        yo=y+i;
-        xo=x;
-        line([x xo],[y yo],'Linewidth',6)
-        pause(0.5);
+    if(level==0)
+        x_current = [0];
+        y_current = [0];
+        y_current(1)=y(1)+i;
+        x_current(1)=x(1);
+        line([x(1) x_current(1)],[y(1) y_current(1)],'Linewidth',6)
+        pause(0.1);
         i=i+1;
         length=length+1;
     else
-        xo=x+i;
-        yo=y+i*tan(gradient);
-        line([x xo],[y yo],'Linewidth',6)
-        pause(0.5);
-        i=i+1;
-        length=i/cos(gradient);
+        break;
     end
-   
+end
+    
+i = 0;
+proceed = 1;
+    
+if level>0
+    while(proceed)
+            count = 0;
+            for j = 1 : size(theta,2)
+                k = 1;
+                angle = theta(j);
+                gradient = abs(tan(angle));
+                if (angle > pi/2)
+                    k = -1;
+                end
+                x_previous(j)= x_current(j);
+                y_previous(j)= y_current(j);
 
+                length_current(j) = abs(i/cos(angle));
+
+                if(length_current(j)<8)
+                    x_current(j) = x(floor((j+1)/2))+ k*i;
+                    y_current(j) = y(floor((j+1)/2))+ i*gradient;
+                else
+                    count = count + 1;
+                end
+
+            end
+
+            i=i+1;
+            if count==size(theta,2)
+                proceed = 0;
+            end
+
+            for m = 1 : size(x_current,2)
+                line([x_previous(m) x_current(m)],[y_previous(m) y_current(m)],'Linewidth',6)
+                pause(0.1);
+            end
+    end
 end
 
+        
+   
 
-theta1 = gradient+pi/4;
-theta2 = gradient-pi/4;
+
 
 
--- sim_gene_reg.m
+++ sim_gene_reg.m
@@ -3,14 +3,29 @@ close all;
 
 x0=5;
 y0=5;
-width=1050;
-height=1000;
+width=500;
+height=500;
 set(gcf,'units','points','position',[x0,y0,width,height])
 xlim([-50 50])
 ylim([0 100])
 
-[theta1,theta2,x1,y1]=growing(0,0,pi/2);
-growing(x1,y1,theta2);
+x(1) = 0;
+y(1) = 0;
+theta(1) = pi/2;
+
+
+[theta1,x1,y1]=growing(x,y,theta,0);
+
+
+[theta2,x2,y2]=growing(x1,y1,theta1,1);
+
+[theta3,x3,y3]=growing(x2,y2,theta2,2);
+
+[theta4,x4,y4]=growing(x3,y3,theta3,3);
+
+[theta5,x5,y5]=growing(x4,y4,theta4,4);
+
+
 
 
 
